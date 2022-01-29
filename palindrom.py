3d7353fe37889aef9b4758fe5d51cdf6f2cbf05b
STARSZYPAN <98351502+STARSZYPAN@users.noreply.github.com>
2022-01-29 16:11:00 +0100

    adde solution

--- palindrom.py
+++ palindrom.py
@@ -3,3 +3,8 @@ def Czy_palindrom(x):
     for i in range(n-1):
         if x[i] != x[n-1-i]:
             return False
+    return True
+print("Program do spradszania czy słowo jest palondromem")
+print("Podaj słowo: ")
+s1 = input()
+print("Podane słowo " + ("jest" if(Czy_palindrom(s1)) else "nie jest") + " palindromem")
